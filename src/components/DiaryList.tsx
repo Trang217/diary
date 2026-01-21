99a6871c1bf8f5d15da8bf7f564d65100c66dde1
Trang <nguyenminhtrang9213@gmail.com>
2026-01-21 08:34:43 +0100

    add delete diary

--- src/components/DiaryList.tsx
+++ src/components/DiaryList.tsx
@@ -26,6 +26,13 @@ function DiaryList() {
         setDiaryEntry(null);
     };
 
+    const onDeleteDiary = (id: string) => {
+        const newDiaries = diaries.filter(diary => diary.id !== id);
+        setDiaries(newDiaries);
+        localStorage.setItem('diaries', JSON.stringify(newDiaries));
+        closeDiary();
+    };
+
     return (
         <>
             {!diaryEntry && (
@@ -42,6 +49,12 @@ function DiaryList() {
                             <h4>Created at {diary.createdAt}</h4>
                         </div>
                     ))}
+                </div>
+            )}
+
+            {diaries.length === 0 && (
+                <div className="w-full rounded-xl bg-amber-50 p-3 text-center font-medium">
+                    There is no diary yet
                 </div>
             )}
 
@@ -71,7 +84,12 @@ function DiaryList() {
                     </h3>
                     <p className="text-lg">{diaryEntry.body}</p>
                     <div className="flex justify-between gap-2">
-                        <button className="btn mt-2 flex-1">Delete</button>
+                        <button
+                            className="btn mt-2 flex-1"
+                            onClick={() => onDeleteDiary(diaryEntry.id)}
+                        >
+                            Delete
+                        </button>
                         <button
                             className="btn btn-secondary mt-2 flex-1"
                             onClick={closeDiary}
