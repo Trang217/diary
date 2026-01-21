459a7e19b20bef32b445a730641914375efddfae
Trang <nguyenminhtrang9213@gmail.com>
2026-01-21 08:11:43 +0100

    add close diary

--- src/components/DiaryList.tsx
+++ src/components/DiaryList.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { ImHappy2, ImNeutral2, ImSad2 } from 'react-icons/im';
 
 type Diary = {
     id: string;
@@ -15,21 +16,71 @@ function DiaryList() {
         return diaryList;
     });
 
+    const [diaryEntry, setDiaryEntry] = useState<Diary | null>(null);
+
+    const onViewDiary = (diary: Diary) => {
+        setDiaryEntry(diary);
+    };
+
+    const closeDiary = () => {
+        setDiaryEntry(null);
+    };
+
     return (
         <>
-            <div className="grid grid-cols-2 gap-4">
-                {diaries.map(diary => (
-                    <div
-                        className="rounded-lg bg-white p-4 transition-all duration-300 ease-in-out hover:scale-105 hover:cursor-pointer hover:shadow-xl"
-                        key={diary.id}
-                    >
-                        <h3 className="mb-2 text-xl font-bold capitalize">
-                            {diary.title}
-                        </h3>
-                        <h4>Created at {diary.createdAt}</h4>
+            {!diaryEntry && (
+                <div className="grid grid-cols-2 gap-4">
+                    {diaries.map(diary => (
+                        <div
+                            className="rounded-lg bg-white p-4 transition-all duration-300 ease-in-out hover:scale-105 hover:cursor-pointer hover:shadow-xl"
+                            key={diary.id}
+                            onClick={() => onViewDiary(diary)}
+                        >
+                            <h3 className="mb-2 text-xl font-bold capitalize">
+                                {diary.title}
+                            </h3>
+                            <h4>Created at {diary.createdAt}</h4>
+                        </div>
+                    ))}
+                </div>
+            )}
+
+            {diaryEntry && (
+                <div className="relative flex max-h-160 flex-col overflow-y-scroll rounded-xl bg-white p-8">
+                    {diaryEntry.emotion === 'Sad' ? (
+                        <ImSad2
+                            size={128}
+                            className="absolute -top-3 -right-8 z-0 text-gray-200"
+                        />
+                    ) : diaryEntry.emotion === 'Happy' ? (
+                        <ImHappy2
+                            size={128}
+                            className="absolute -top-3 -right-8 z-0 text-gray-200"
+                        />
+                    ) : (
+                        <ImNeutral2
+                            size={128}
+                            className="absolute -top-3 -right-10 z-0 text-gray-200"
+                        />
+                    )}
+                    <h2 className="mb-2 text-4xl font-bold">
+                        {diaryEntry.title}
+                    </h2>
+                    <h3 className="mb-4 text-gray-500">
+                        Created at {diaryEntry.createdAt}
+                    </h3>
+                    <p className="text-lg">{diaryEntry.body}</p>
+                    <div className="flex justify-between gap-2">
+                        <button className="btn mt-2 flex-1">Delete</button>
+                        <button
+                            className="btn btn-secondary mt-2 flex-1"
+                            onClick={closeDiary}
+                        >
+                            Close
+                        </button>
                     </div>
-                ))}
-            </div>
+                </div>
+            )}
         </>
     );
 }
